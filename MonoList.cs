593f739332e00b3375fef56ea99edf5725220f12
FilaCo <FilaCo159@yandex.ru>
2019-06-02 12:59:20 +0500

    Removed ListParams and added MonoList<T>

---
+++ MonoList.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace LabSOLID
+{
+    /// <summary>
+    /// Represents a mono-direction list
+    /// </summary>
+    public class MonoList <T> : IEnumerable<T>
+    {
+        private MonoList<T> _next;
+
+        private readonly T _data;
+
+        public MonoList (T data)
+        {
+            _data = data;
+        }
+        
+        public void AddEnd(T data)
+        {
+            var cur = this;
+            
+            while (cur._next != null)
+                cur = cur._next;
+            
+            cur._next = new MonoList<T>(data);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var cur = this;
+
+            while (!(cur is null))
+            {
+                yield return cur._data;
+                
+                cur = cur._next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
