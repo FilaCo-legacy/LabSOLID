d274f6dc4c0ede242ab385d7925961f15d46d5fc
FilaCo <FilaCo159@yandex.ru>
2019-06-02 16:19:09 +0500

    Reimpleneted for second parsers hierarchy

--- ParsingStructs/ValueParsers/BaseValueParser.cs
+++ ParsingStructs/ValueParsers/BaseValueParser.cs
@@ -2,18 +2,16 @@ using System;
 
 namespace LabSOLID.ParsingStructs.ValueParsers
 {
-    public abstract class BaseValueParser : IValueParser
+    public abstract class BaseValueParser : MonoList<IValueParser>, IValueParser
     {
         protected abstract string Pattern { get; }
 
         protected bool CanHandle(string source) => Pattern != source;
-
-        public IValueParser Next { get; set; }
         
         public virtual Value Parse(string source)
         {
-            if (Next != null)
-                return Next.Parse(source);
+            if (_next != null)
+                return _next.Data.Parse(source);
             
             throw new Exception("Undefined value type");
         }
--- ParsingStructs/ValueParsers/IValueParser.cs
+++ ParsingStructs/ValueParsers/IValueParser.cs
@@ -2,8 +2,6 @@ namespace LabSOLID.ParsingStructs.ValueParsers
 {
     public interface IValueParser
     {
-        IValueParser Next { get; set; }
-
         Value Parse(string source);
     }
 }
